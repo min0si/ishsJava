f2f56eb96034a63e8990b4e1f80d45ffe634a2b6
inhadeepblue <deepblue@inha.ac.kr>
2025-10-16 20:26:36 +0900

    v0.4 Pokemon game

--- week06/src/GameDemo.java
+++ week06/src/GameDemo.java
@@ -1,10 +1,13 @@
 public class GameDemo {
     public static void main(String[] args) {
-        Pikachu myPikachu = new Pikachu(100);
-        Pikachu yourPikachu = new Pikachu(100);
-        Squirtle enemySquirtle = new Squirtle(120);
+        Pikachu myPikachu = new Pikachu(100, 27);
+        Pikachu yourPikachu = new Pikachu(100, 28);
+        Squirtle enemySquirtle = new Squirtle(120, 21);
         System.out.println("Game start~");
         myPikachu.attack(enemySquirtle);
         enemySquirtle.attack(myPikachu);
+
+        System.out.println(enemySquirtle);
+        System.out.println(yourPikachu);
     }
 }
--- week06/src/Squirtle.java
+++ week06/src/Squirtle.java
@@ -1,6 +1,6 @@
 public class Squirtle extends Pokemon {
-    public Squirtle(int hp){
-        super("꼬부기", hp);  // 부모클래스의 생성자 호출
+    public Squirtle(int hp, int attackPower){
+        super("꼬부기", hp, attackPower);  // 부모클래스의 생성자 호출
         System.out.println("꼬북꼬북!");
     }
 
